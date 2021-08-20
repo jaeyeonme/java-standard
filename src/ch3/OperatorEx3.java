a6ef9fab47cc1299206d01238c1659467496feb8
JaeYeon33 <cjyeon1022@gmail.com>
2021-08-20 23:55:32 +0900

    2021-08-20 java study files

---
+++ src/ch3/OperatorEx3.java
@@ -0,0 +1,17 @@
+package ch3;
+
+/**
+ * Date   : 2021. 08. 20
+ * Source : OperatorEx3.java
+ * Author : 조재연 (Jaeyeon Cho)
+ * Email  : cjyeon1022@gamil.com
+ */
+class OperatorEx3 {
+    public static void main(String[] args) {
+
+        int i = 5, j = 5;
+        System.out.println(i++);
+        System.out.println(++j);
+        System.out.println("i = " + i + ", j = " + j);
+    }
+}
