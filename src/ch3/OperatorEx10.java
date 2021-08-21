9fb31ce6b0eedd0021fec14de970e48058f3b914
JaeYeon33 <cjyeon1022@gmail.com>
2021-08-21 23:59:29 +0900

    2021-08-21 java study files

---
+++ src/ch3/OperatorEx10.java
@@ -0,0 +1,20 @@
+package ch3;
+
+/**
+ * Date   : 2021. 08. 21
+ * Source : OperatorEx10.java
+ * Author : 조재연 (Jaeyeon Cho)
+ * Email  : cjyeon1022@gamil.com
+ */
+class OperatorEx10 {
+    public static void main(String[] args) {
+
+        int a = 1000000;
+
+        int result1 = a * a / a;
+        int result2 = a / a * a;
+
+        System.out.printf("%d * %d / %d = %d\n", a, a, a, result1);
+        System.out.printf("%d / %d & %d = %d\n", a, a, a, result2);
+    }
+}
