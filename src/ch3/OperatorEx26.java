1869905199b460c1d799f2b5c693ec0737326b0d
JaeYeon33 <cjyeon1022@gmail.com>
2021-08-24 02:59:04 +0900

    2021-08-24 java study files

---
+++ src/ch3/OperatorEx26.java
@@ -0,0 +1,22 @@
+package ch3;
+
+/**
+ * Date   : 2021. 08. 24
+ * Source : OperatorEx26.java
+ * Author : 조재연 (Jaeyeon Cho)
+ * Email  : cjyeon1022@gamil.com
+ */
+class OperatorEx26 {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 0;
+
+        System.out.printf("a = %d, b = %d\n", a, b);
+        System.out.printf("a != 0 || ++b != 0 = %b\n", a != 0 || ++b != 0);
+        System.out.printf("a = %d, b = %d\n", a, b);
+        System.out.printf("a == 0 && ++b != 0 = %b\n", a == 0 && ++b != 0);
+        System.out.printf("a = %d, b = %d\n", a, b);
+
+    }
+}
