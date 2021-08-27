9afba5c5469dcb1814c5811857ac20682d294cbc
JaeYeon33 <cjyeon1022@gmail.com>
2021-08-27 23:44:14 +0900

    2021-08-27 java study files

---
+++ src/ch4/FlowEx10.java
@@ -0,0 +1,43 @@
+package ch4;
+
+import java.util.Scanner;
+
+/**
+ * Date   : 2021. 08. 27
+ * Source : FlowEx10.java
+ * Author : 조재연 (Jaeyeon Cho)
+ * Email  : cjyeon1022@gamil.com
+ */
+class FlowEx10 {
+    public static void main(String[] args) {
+
+        int score = 0;
+        char grade = ' ';
+
+        System.out.print("당신의 점수를 입력하세요. (1~100) > ");
+
+        Scanner scanner = new Scanner(System.in);
+        String tmp = scanner.nextLine();
+        score = Integer.parseInt(tmp);
+
+        switch (score / 10) {
+            case 10:
+            case 9:
+                grade = 'A';
+                break;
+
+            case 8:
+                grade = 'B';
+                break;
+
+            case 7:
+                grade = 'C';
+                break;
+
+            default:
+                grade= 'F';
+        }
+
+        System.out.println("당신의 학점은 " + grade + "입니다.");
+    }
+}
---
+++ src/ch4/FlowEx8.java
@@ -0,0 +1,34 @@
+package ch4;
+
+import java.util.Scanner;
+
+/**
+ * Date   : 2021. 08. 27
+ * Source : FlowEx8.java
+ * Author : 조재연 (Jaeyeon Cho)
+ * Email  : cjyeon1022@gamil.com
+ */
+class FlowEx8 {
+    public static void main(String[] args) {
+
+        System.out.print("당신의 주민번호를 입력하세요. (011231-1111222) > ");
+
+        Scanner scanner = new Scanner(System.in);
+        String regNo = scanner.nextLine();
+
+        char gender = regNo.charAt(7); // 입력받은 번호의 8번째 문자를 gender에 저장
+
+        switch (gender) {
+            case '1': case '3':
+                System.out.println("당신은 남자입니다.");
+                break;
+
+            case '2': case '4':
+                System.out.println("당신은 여자입니다.");
+                break;
+
+            default:
+                System.out.println("유효하지 않는 주민번호입니다.");
+        }
+    }
+}
